41d62443745396cdd64ee8a8cc7a6755437c84b3
Robin Ince <robince@gmail.com>
2008-09-24 11:53:46 +0000

    add distutils setup.py script

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='pyentropy',
+      version='0.2b',
+      description='Entropy and Information Theoretic Estimates',
+      author='Robin Ince',
+      author_email='pyentropy@robince.net',
+      url='http://code.google.com/p/pyentropy',
+      py_modules=['pyentropy']
+      )
