eb2563116a529afb40edc819643c854edbfd23e1
Robin Ince <robince@gmail.com>
2008-09-24 12:09:33 +0000

    add unittest framework

---
+++ test_pyentropy.py
@@ -0,0 +1,19 @@
+import pyentropy
+import unittest
+
+class DiscreteSystemTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # randomly generate data
+        
+        # initialise DiscreteSystem class
+        
+        pass
+
+    def tearDown(self):
+        # self.sys = None
+        pass
+
+if __name__ '__main__':
+    unittest.main()
+
