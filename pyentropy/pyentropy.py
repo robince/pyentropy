fix serious bugs with PiX construction - decimalise ordering and averaging over Y

pyentropy/pyentropy.py
@@ -461,12 +461,13 @@ class DiscreteSystem(BaseSystem):
                                 self.Xsh[j,indx] = shfoce
         # Pind(X) = <Pind(X|Y)>_y
         if ('HiX' in calc) or ('ChiX' in calc):
-            # average over Y
-            PiXi = np.dot(self.PXiY, self.PY)
             # construct joint distribution
             words = dec2base(np.atleast_2d(np.r_[0:self.X_dim]).T,self.X_m,self.X_n)
-            self.PiX = PiXi[words,np.r_[0:self.X_n]].prod(axis=1)
-                            
+            PiXY = np.zeros((self.X_dim, self.Y_dim))
+            PiXY = self.PXiY[words,np.r_[0:self.X_n]].prod(axis=1)
+            # average over Y
+            self.PiX = np.dot(PiXY,self.PY)
+
         self.sampled = True
 
     def _check_inputs(self, X, Y):
@@ -776,7 +777,7 @@ def decimalise(x, n, m):
 
     """
     #TODO: Error checking?
-    powers = m**np.arange(0,n,dtype=int)
+    powers = m**np.arange(0,n,dtype=int)[::-1]
     #self.powers = self.powers[::-1]
     d_x = np.dot(x.T,powers).astype(int)
 