d21d93ef423f0c75ff058fb7624c7f1804fc9f5a
Robin Ince <robince@gmail.com>
2009-12-15 19:25:15 +0000

    update version for 0.4.0

--- pyentropy/__init__.py
+++ pyentropy/__init__.py
@@ -24,7 +24,7 @@ http://code.google.com/p/pyentropy
 """
 
 __author__ = 'Robin Ince'
-__version__ = '0.4.0dev'
+__version__ = '0.4.0'
 
 from systems import DiscreteSystem, SortedDiscreteSystem
 from utils import (prob, decimalise, nsb_entropy, quantise,
